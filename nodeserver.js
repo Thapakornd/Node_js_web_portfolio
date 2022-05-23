e23e78e9119b517dd9d1a21ea7198f9a758559b4
Thapakornd <89478647+Thapakornd@users.noreply.github.com>
2022-05-23 16:39:12 +0700

    test nodejs server

--- nodeserver.js
+++ nodeserver.js
@@ -6,6 +6,15 @@ const port = 3000
 
 //use view ejs : npm i ejs
 app.set('view engine','ejs')
+//Give app to use function
+//app.use(logger)
+
+//Give app to use public folder
+//If use static public in views can't render
+//in public folder you can use much u want application
+app.use(express.static("public"))
+app.use(express.urlencoded({ extended: true}))
+app.use(express.json())
 
 //link to index page
 app.get('/',(req,res) => {
@@ -15,8 +24,12 @@ app.get('/',(req,res) => {
 
 //link router to user.js
 const userRouter = require('./routes/users')
-
 app.use('/users', userRouter)
+
+function logger(req,res,next) {
+    console.log(req.originalUrl)
+    next()
+}
 
 //Open Server  
 app.listen(port)
--- routes/users.js
+++ routes/users.js
@@ -2,24 +2,43 @@ const express = require('express')
 const router = express.Router()
 
 router.get("/", (req,res) => {
+    console.log(req.query.name)
     res.send("User List")
 })
 
 router.post('/',(req,res) => {
-    res.send('Create user')
+    const isValid = true
+    if (isValid){
+        users.push({ firstname: req.body.firstname})
+        res.redirect(`/users/$(users.length) - 1`)
+    }else{
+        console.log("ERROR")
+        res.render("users/new",{ firstname: req.body.firstname})
+    }
+})
+
+router.get('/new',(req,res) => {
+    res.render("users/new" , { firstname: "Test"}) 
 })
 
 router
     .route("/:id")
     .get((req,res) => {
-    res.send(`Get your ID: ${req.params.id}`)
+        console.log(req.user)
+        res.send(`Get your ID: ${req.params.id}`)
     })
     .put((req,res) => {
-    res.send(`Update your ID: ${req.params.id}`)
-})
+        res.send(`Update your ID: ${req.params.id}`)
+    })
+    .delete((req,res) => {
+        res.send(`Delete your ID: ${req.params.id}`)
+    })
 
-router.delete('/:id',(req,res) => {
-    res.send(`Delete your ID: ${req.params.id}`)
+const users = [{name: "Kayle"},{ name: "Sally"}]
+router.param("id",(req,res,next,id) => {
+    console.log(id)
+    req.user = users[id]
+    next()
 })
 
 module.exports = router
